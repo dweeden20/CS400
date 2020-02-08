6c817465c95483ebfd52d74ef39336f30dab0518
Dean <dweeden@bu.edu>
2020-02-07 19:28:43 -0500

    issues with problem 1

--- PS2PR1.js
+++
@@ -1,7 +0,0 @@
-/*
-
-    Write two generators that together implement a series of even Fibonacci numbers. The first
-    generator should return the series of fibonacci numbers starting from 0. The series F is defined
-
-
-*/
